edfabd53b54463cd313a1e05168327695d4b4f95
耗子 <i@haozi.net>
2023-06-30 20:11:46 +0800

    feat: optimize server provider

--- service_provider.go
+++ service_provider.go
@@ -29,6 +29,10 @@ type ServiceProvider struct {
 
 func (receiver *ServiceProvider) Register(app foundation.Application) {
 	App = app
+	ConfigFacade = app.MakeConfig()
+	CacheFacade = app.MakeCache()
+	LogFacade = app.MakeLog()
+	ValidationFacade = app.MakeValidation()
 
 	app.Bind(HttpBinding, func(app foundation.Application) (any, error) {
 		return NewFiberContext(&fiber.Ctx{}), nil
@@ -39,8 +43,4 @@ func (receiver *ServiceProvider) Register(app foundation.Application) {
 }
 
 func (receiver *ServiceProvider) Boot(app foundation.Application) {
-	ConfigFacade = app.MakeConfig()
-	CacheFacade = app.MakeCache()
-	LogFacade = app.MakeLog()
-	ValidationFacade = app.MakeValidation()
 }
