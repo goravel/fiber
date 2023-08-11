4d82bc6c1e8107d690c6335b0dcddcad7de16eed
耗子 <i@haozi.net>
2023-08-11 12:30:32 +0800

    chore: remove unnecessary values

--- service_provider.go
+++ service_provider.go
@@ -3,10 +3,8 @@ package fiber
 import (
 	"github.com/gofiber/fiber/v2"
 
-	"github.com/goravel/framework/contracts/cache"
 	"github.com/goravel/framework/contracts/config"
 	"github.com/goravel/framework/contracts/foundation"
-	"github.com/goravel/framework/contracts/http"
 	"github.com/goravel/framework/contracts/log"
 	"github.com/goravel/framework/contracts/validation"
 )
@@ -17,11 +15,9 @@ const RouteBinding = "goravel.route"
 var App foundation.Application
 
 var (
-	ConfigFacade      config.Config
-	CacheFacade       cache.Cache
-	LogFacade         log.Log
-	RateLimiterFacade http.RateLimiter
-	ValidationFacade  validation.Validation
+	ConfigFacade     config.Config
+	LogFacade        log.Log
+	ValidationFacade validation.Validation
 )
 
 type ServiceProvider struct {
@@ -40,7 +36,6 @@ func (receiver *ServiceProvider) Register(app foundation.Application) {
 }
 
 func (receiver *ServiceProvider) Boot(app foundation.Application) {
-	CacheFacade = app.MakeCache()
 	LogFacade = app.MakeLog()
 	ValidationFacade = app.MakeValidation()
 }
