c88f2801e57668ff67b8262ed8c55d4ffdfbf95c
耗子 <i@haozi.net>
2023-08-11 12:36:56 +0800

    fix: tests

--- service_provider.go
+++ service_provider.go
@@ -3,6 +3,7 @@ package fiber
 import (
 	"github.com/gofiber/fiber/v2"
 
+	"github.com/goravel/framework/contracts/config"
 	"github.com/goravel/framework/contracts/foundation"
 	"github.com/goravel/framework/contracts/log"
 	"github.com/goravel/framework/contracts/validation"
@@ -14,6 +15,7 @@ const RouteBinding = "goravel.route"
 var App foundation.Application
 
 var (
+	ConfigFacade     config.Config
 	LogFacade        log.Log
 	ValidationFacade validation.Validation
 )
@@ -33,6 +35,7 @@ func (receiver *ServiceProvider) Register(app foundation.Application) {
 }
 
 func (receiver *ServiceProvider) Boot(app foundation.Application) {
+	ConfigFacade = app.MakeConfig()
 	LogFacade = app.MakeLog()
 	ValidationFacade = app.MakeValidation()
 }
