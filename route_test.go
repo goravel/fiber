55d4cea06aace7c1d50300dc3c0c5e297a733334
耗子 <i@haozi.net>
2023-08-18 12:39:39 +0800

    fix: tests

--- route_test.go
+++ route_test.go
@@ -886,7 +886,7 @@ func TestRequest(t *testing.T) {
 				})
 
 				payload := strings.NewReader(`{
-					"id": {"a": "4"}
+					"id": {"a": "3"}
 				}`)
 				req, _ = http.NewRequest(method, url, payload)
 				req.Header.Set("Content-Type", "application/json")
