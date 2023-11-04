c843853f962b5f70eccace4fc1566d23d0166477
耗子 <i@haozi.net>
2023-11-04 17:42:38 +0800

    feat: add escape

--- group.go
+++ group.go
@@ -2,6 +2,8 @@ package fiber
 
 import (
 	"net/http"
+	"net/url"
+	"path/filepath"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/filesystem"
@@ -103,9 +105,13 @@ func (r *Group) Static(relativePath, root string) {
 	r.clearMiddlewares()
 }
 
-func (r *Group) StaticFile(relativePath, filepath string) {
+func (r *Group) StaticFile(relativePath, filePath string) {
 	r.instance.Use(r.getMiddlewaresWithPath(relativePath, nil)...).Use(r.getPath(relativePath), func(c *fiber.Ctx) error {
-		return c.SendFile(filepath, true)
+		dir, file := filepath.Split(filePath)
+		escapedFile := url.PathEscape(file)
+		escapedPath := filepath.Join(dir, escapedFile)
+
+		return c.SendFile(escapedPath, true)
 	})
 	r.clearMiddlewares()
 }
--- response.go
+++ response.go
@@ -1,6 +1,9 @@
 package fiber
 
 import (
+	"net/url"
+	"path/filepath"
+
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -32,7 +35,11 @@ type FileResponse struct {
 }
 
 func (r *FileResponse) Render() error {
-	return r.instance.SendFile(r.filepath, true)
+	dir, file := filepath.Split(r.filepath)
+	escapedFile := url.PathEscape(file)
+	escapedPath := filepath.Join(dir, escapedFile)
+
+	return r.instance.SendFile(escapedPath, true)
 }
 
 type JsonResponse struct {
