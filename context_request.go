63c03f977cab189b44469b990221045444d45c18
Bowen <hwbrzzl@gmail.com>
2024-07-02 16:55:48 +0800

    fix:[#450] The image rule of validation is wrong

--- context_request.go
+++ context_request.go
@@ -385,26 +385,29 @@ func (r *ContextRequest) Validate(rules map[string]string, options ...contractsv
 	}
 
 	options = append(options, validation.Rules(rules), validation.CustomRules(r.validation.Rules()))
-	generateOptions := validation.GenerateOptions(options)
 
-	var v *validate.Validation
-	dataFace := validate.FromMap(r.ctx.Request().All())
+	dataFace, err := validate.FromRequest(r.ctx.Request().Origin())
+	if err != nil {
+		return nil, err
+	}
 
-	if generateOptions["prepareForValidation"] != nil {
-		if err := generateOptions["prepareForValidation"].(func(ctx contractshttp.Context, data contractsvalidate.Data) error)(r.ctx, validation.NewData(dataFace)); err != nil {
-			return nil, err
+	for key, query := range r.instance.Queries() {
+		if _, exist := dataFace.Get(key); !exist {
+			if _, err := dataFace.Set(key, query); err != nil {
+				return nil, err
+			}
 		}
 	}
 
-	v = dataFace.Create()
-
-	if generateOptions["filters"] != nil {
-		v.FilterRules(generateOptions["filters"].(map[string]string))
+	for key, param := range r.instance.AllParams() {
+		if _, exist := dataFace.Get(key); !exist {
+			if _, err := dataFace.Set(key, param); err != nil {
+				return nil, err
+			}
+		}
 	}
 
-	validation.AppendOptions(v, generateOptions)
-
-	return validation.NewValidator(v), nil
+	return r.validation.Make(dataFace, rules, options...)
 }
 
 func (r *ContextRequest) ValidateRequest(request contractshttp.FormRequest) (contractsvalidate.Errors, error) {
