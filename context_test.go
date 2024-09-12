9989eb4ce2ee4875e23cb85080f647f2f038b05e
Danial <mdanialrma@gmail.com>
2024-09-12 09:30:35 +0700

    test: merge test cases

--- context_test.go
+++ context_test.go
@@ -7,10 +7,19 @@ import (
 )
 
 func TestContext(t *testing.T) {
+	type customKeyType struct{}
+	var customKey customKeyType
+
 	httpCtx := Background()
 	httpCtx.WithValue("Hello", "world")
 	httpCtx.WithValue("Hi", "Goravel")
+	httpCtx.WithValue(1, "one")
+	httpCtx.WithValue(1.1, "one point one")
+	httpCtx.WithValue(customKey, "hello")
 
 	assert.Equal(t, httpCtx.Value("Hello").(string), "world")
 	assert.Equal(t, httpCtx.Value("Hi").(string), "Goravel")
+	assert.Equal(t, httpCtx.Value(1).(string), "one")
+	assert.Equal(t, httpCtx.Value(1.1).(string), "one point one")
+	assert.Equal(t, httpCtx.Value(customKey), "hello")
 }
