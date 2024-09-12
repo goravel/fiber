6fa44507cf10c88f8ba5a2ef2668c95dd384b6c4
Danial <mdanialrma@gmail.com>
2024-09-12 23:57:47 +0700

    test: more test cases for WithValue and Context

--- context_test.go
+++ context_test.go
@@ -7,10 +7,26 @@ import (
 )
 
 func TestContext(t *testing.T) {
+	type customKeyType struct{}
+	var customKey customKeyType
+
 	httpCtx := Background()
 	httpCtx.WithValue("Hello", "world")
 	httpCtx.WithValue("Hi", "Goravel")
+	httpCtx.WithValue(customKey, "halo")
+	httpCtx.WithValue(1, "one")
+	httpCtx.WithValue(2.2, "two point two")
 
 	assert.Equal(t, httpCtx.Value("Hello").(string), "world")
 	assert.Equal(t, httpCtx.Value("Hi").(string), "Goravel")
+	assert.Equal(t, "halo", httpCtx.Value(customKey))
+	assert.Equal(t, "one", httpCtx.Value(1))
+	assert.Equal(t, "two point two", httpCtx.Value(2.2))
+
+	ctx := httpCtx.Context()
+	assert.Equal(t, "world", ctx.Value("Hello"))
+	assert.Equal(t, "Goravel", ctx.Value("Hi"))
+	assert.Equal(t, "halo", ctx.Value(customKey))
+	assert.Equal(t, "one", ctx.Value(1))
+	assert.Equal(t, "two point two", ctx.Value(2.2))
 }
