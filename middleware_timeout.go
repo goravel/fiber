9020aa4549714359297303d4f66af19815e6c919
Bowen <hwbrzzl@gmail.com>
2024-11-11 17:38:30 +0800

    add comment

--- middleware_timeout.go
+++ middleware_timeout.go
@@ -9,7 +9,9 @@ import (
 	contractshttp "github.com/goravel/framework/contracts/http"
 )
 
-// Timeout creates middleware to set a timeout for a request
+// Timeout creates middleware to set a timeout for a request.
+// NOTICE: It does not cancel long running executions. Underlying executions must handle timeout by using context.Context parameter.
+// For details, see https://github.com/valyala/fasthttp/issues/965
 func Timeout(timeout time.Duration) contractshttp.Middleware {
 	return func(ctx contractshttp.Context) {
 		timeoutCtx, cancel := context.WithTimeout(ctx.Context(), timeout)
