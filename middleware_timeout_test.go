84f834ad3964dcbc64c798076e98bda71a7c95fa
Evgeny <113383200+KlassnayaAfrodita@users.noreply.github.com>
2024-12-29 02:25:34 +0300

    Update middleware_timeout_test.go

--- middleware_timeout_test.go
+++ middleware_timeout_test.go
@@ -11,7 +11,6 @@ import (
 	mocksconfig "github.com/goravel/framework/mocks/config"
 	mockslog "github.com/goravel/framework/mocks/log"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 )
 
