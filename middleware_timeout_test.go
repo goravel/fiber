ae56b4f984201ebe3c6233a7bd206aa584581bfd
Bowen <hwbrzzl@gmail.com>
2024-12-30 11:06:40 +0800

    optimize

--- middleware_timeout_test.go
+++ middleware_timeout_test.go
@@ -17,9 +17,9 @@ import (
 
 func TestTimeoutMiddleware(t *testing.T) {
 	mockConfig := mocksconfig.NewConfig(t)
-	mockConfig.On("GetBool", "http.drivers.fiber.prefork", false).Return(false).Once()
-	mockConfig.On("GetInt", "http.drivers.fiber.body_limit", 4096).Return(4096).Once()
-	mockConfig.On("GetInt", "http.drivers.fiber.header_limit", 4096).Return(4096).Once()
+	mockConfig.EXPECT().GetBool("http.drivers.fiber.prefork", false).Return(false).Once()
+	mockConfig.EXPECT().GetInt("http.drivers.fiber.body_limit", 4096).Return(4096).Once()
+	mockConfig.EXPECT().GetInt("http.drivers.fiber.header_limit", 4096).Return(4096).Once()
 
 	route, err := NewRoute(mockConfig, nil)
 	require.NoError(t, err)
@@ -68,9 +68,9 @@ func TestTimeoutMiddleware(t *testing.T) {
 
 	t.Run("panic with default recover", func(t *testing.T) {
 		mockLog := mockslog.NewLog(t)
-		mockLog.On("WithContext", mock.Anything).Return(mockLog).Once()
-		mockLog.On("Request", mock.Anything).Return(mockLog).Once()
-		mockLog.On("Error", "test panic").Once()
+		mockLog.EXPECT().WithContext(mock.Anything).Return(mockLog).Once()
+		mockLog.EXPECT().Request(mock.Anything).Return(mockLog).Once()
+		mockLog.EXPECT().Error("test panic").Once()
 		LogFacade = mockLog
 
 		req, err := http.NewRequest("GET", "/panic", nil)
